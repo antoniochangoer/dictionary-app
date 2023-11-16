1c52ffd4dbb29312e2857dec869c6378ac6e845d
antoniochangoer <54006126+antoniochangoer@users.noreply.github.com>
2023-11-16 22:08:18 +0100

    link wrap

--- src/components/Source.jsx
+++ src/components/Source.jsx
@@ -5,7 +5,7 @@ export default function Source({ urls }) {
   return (
     <footer className="mt-6 text-sm md:flex">
       <p className="text-brand-light-500 md:mr-5">Source</p>
-      <div className="flex">
+      <div className="flex flex-wrap">
         {urls.map((url, index) => (
           <Fragment key={index}>
             <a
